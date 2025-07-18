c64b195e37263e7f26e04110c722bb4a539466de
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-07-18 10:05:48 +0530

    Update RegisterDetailsRepository.java

--- src/main/java/com/example/springbootfirst/repository/RegisterDetailsRepository.java
+++ src/main/java/com/example/springbootfirst/repository/RegisterDetailsRepository.java
@@ -4,6 +4,7 @@ import com.example.springbootfirst.models.RegisterDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,6 @@ public interface RegisterDetailsRepository extends JpaRepository<RegisterDetails
     RegisterDetails findByEmail(String email);
 
     Optional<RegisterDetails> findByUserName(String username);
+
+    List<RegisterDetails> findByUserNameContainingIgnoreCase(String userName); // used in search
 }
